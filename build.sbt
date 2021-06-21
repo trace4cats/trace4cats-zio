a7b3f40d7bcb87a32f3cd4af569dc54624cd103d
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-06-21 18:40:29 +0300

    Unify build, workflows

--- build.sbt
+++ build.sbt
@@ -1,23 +1,22 @@
 lazy val commonSettings = Seq(
-  libraryDependencies += compilerPlugin(("org.typelevel" %% "kind-projector" % "0.13.0").cross(CrossVersion.patch)),
+  Compile / compile / javacOptions ++= Seq("-source", "1.8", "-target", "1.8"),
   libraryDependencies ++= {
     CrossVersion.partialVersion(scalaVersion.value) match {
-      case Some((2, _)) => compilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1") :: Nil
-      case _ => Nil
+      case Some((2, _)) =>
+        Seq(compilerPlugin(Dependencies.kindProjector), compilerPlugin(Dependencies.betterMonadicFor))
+      case _ => Seq.empty
     }
   },
-  Compile / compile / javacOptions ++= Seq("-source", "1.8", "-target", "1.8"),
   scalacOptions := {
-    val opts = scalacOptions.value :+ "-Wconf:src=src_managed/.*:s,any:wv"
-
+    val opts = scalacOptions.value
+    val wconf = "-Wconf:any:wv"
     CrossVersion.partialVersion(scalaVersion.value) match {
-      case Some((2, 12)) => opts.filterNot(Set("-Xfatal-warnings"))
+      case Some((2, _)) => opts :+ wconf
       case _ => opts
     }
   },
   Test / fork := true,
   resolvers += Resolver.sonatypeRepo("releases"),
-  ThisBuild / evictionErrorLevel := Level.Warn,
 )
 
 lazy val noPublishSettings =
@@ -42,13 +41,10 @@ lazy val `base-zio` =
     .settings(
       name := "trace4cats-base-zio",
       libraryDependencies ++= Seq(Dependencies.zioInterop, Dependencies.trace4catsBase),
-      libraryDependencies ++= (Dependencies.test ++ Seq(Dependencies.trace4catsBaseLaws)).map(_ % Test)
+      libraryDependencies ++= Seq(Dependencies.trace4catsBaseLaws, Dependencies.trace4catsTestkit).map(_ % Test)
     )
 
 lazy val `inject-zio` = (project in file("modules/inject-zio"))
   .settings(publishSettings)
-  .settings(
-    name := "trace4cats-inject-zio",
-    libraryDependencies ++= Seq(Dependencies.zioInterop, Dependencies.trace4catsInject)
-  )
+  .settings(name := "trace4cats-inject-zio", libraryDependencies ++= Seq(Dependencies.trace4catsInject))
   .dependsOn(`base-zio`)
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,47 +5,23 @@ object Dependencies {
     val scala212 = "2.12.14"
     val scala213 = "2.13.6"
 
-    val trace4cats = "0.12.0-RC1"
+    val trace4cats = "0.12.0-RC1+156-7ea07b63"
 
     val cats = "2.6.1"
     val catsEffect = "3.1.1"
     val zioInterop = "3.1.1.0"
 
-    val catsTestkitScalatest = "2.1.5"
-    val disciplineScalatest = "2.1.5"
-    val discipline = "1.1.5"
-    val scalaCheck = "1.15.4"
-    val scalaCheckShapeless = "1.3.0"
-    val scalaTest = "3.2.9"
+    val kindProjector = "0.13.0"
+    val betterMonadicFor = "0.3.1"
   }
 
   lazy val trace4catsBase = "io.janstenpickle"     %% "trace4cats-base"      % Versions.trace4cats
   lazy val trace4catsBaseLaws = "io.janstenpickle" %% "trace4cats-base-laws" % Versions.trace4cats
   lazy val trace4catsInject = "io.janstenpickle"   %% "trace4cats-inject"    % Versions.trace4cats
+  lazy val trace4catsTestkit = "io.janstenpickle"  %% "trace4cats-testkit"   % Versions.trace4cats
 
   lazy val zioInterop = "dev.zio" %% "zio-interop-cats" % Versions.zioInterop
 
-  lazy val catsLaws = "org.typelevel"             %% "cats-laws"              % Versions.cats
-  lazy val catsEffectLaws = "org.typelevel"       %% "cats-effect-laws"       % Versions.catsEffect
-  lazy val catsEffectTestkit = "org.typelevel"    %% "cats-effect-testkit"    % Versions.catsEffect
-  lazy val catsTestkitScalatest = "org.typelevel" %% "cats-testkit-scalatest" % Versions.catsTestkitScalatest
-  lazy val disciplineScalatest = "org.typelevel"  %% "discipline-scalatest"   % Versions.disciplineScalatest
-  lazy val disciplineCore = "org.typelevel"       %% "discipline-core"        % Versions.discipline
-  lazy val scalacheck = "org.scalacheck"          %% "scalacheck"             % Versions.scalaCheck
-  lazy val scalacheckShapeless =
-    "com.github.alexarchambault"       %% "scalacheck-shapeless_1.15" % Versions.scalaCheckShapeless
-  lazy val scalaTest = "org.scalatest" %% "scalatest"                 % Versions.scalaTest
-
-  lazy val test =
-    Seq(
-      catsLaws,
-      catsEffectLaws,
-      catsEffectTestkit,
-      catsTestkitScalatest,
-      disciplineScalatest,
-      disciplineCore,
-      scalacheck,
-      scalacheckShapeless,
-      scalaTest
-    )
+  lazy val kindProjector = ("org.typelevel" % "kind-projector"     % Versions.kindProjector).cross(CrossVersion.full)
+  lazy val betterMonadicFor = "com.olegpy" %% "better-monadic-for" % Versions.betterMonadicFor
 }
