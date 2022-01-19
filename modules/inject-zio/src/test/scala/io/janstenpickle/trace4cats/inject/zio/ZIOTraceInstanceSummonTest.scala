080573cbf8c79a08bfac3fb8748f02d7563480f4
calvinlfer <calvin.l.fer@gmail.com>
2022-01-19 14:04:40 -0500

    Provide improvements to the zio interop to allow for use with ZLayers along with allowing for direct usage with ZIO without using Tagless Final

--- modules/inject-zio/src/test/scala/io/janstenpickle/trace4cats/inject/zio/ZIOTraceInstanceSummonTest.scala
+++ modules/inject-zio/src/test/scala/io/janstenpickle/trace4cats/inject/zio/ZIOTraceInstanceSummonTest.scala
@@ -3,19 +3,31 @@ package io.janstenpickle.trace4cats.inject.zio
 import io.janstenpickle.trace4cats.Span
 import io.janstenpickle.trace4cats.base.context.Local
 import io.janstenpickle.trace4cats.inject.Trace
+import zio.blocking.Blocking
+import zio.clock.Clock
+import zio.console.Console
 import zio.interop.catz._
-import zio.{Has, RIO, Task, ZEnv}
+import zio.random.Random
+import zio.{Has, RIO, ZEnv}
 
 object ZIOTraceInstanceSummonTest {
-  type F[x] = SpannedRIO[x]
+  type Effect[+A] = RIO[Clock with Blocking, A]
+
+  type F[x] = SpannedRIO[Clock with Blocking with Has[Span[Effect]], x]
   implicitly[Trace[F]]
 
-  type ZSpan = Has[Span[Task]]
+  type ZSpan = Has[Span[Effect]]
   type G[x] = RIO[ZEnv with ZSpan, x]
-  implicit val rioLayeredLocalSpan: Local[G, Span[Task]] = zioProvideSome[ZEnv, ZEnv with ZSpan, Throwable, Span[Task]]
+  implicit val rioLayeredLocalSpan: Local[G, Span[Effect]] =
+    zioProvideSome[ZEnv, ZEnv with ZSpan, Throwable, Span[Effect]]
+
   implicitly[Trace[G]]
 
-  type H[x] = RIO[Env, x]
-  implicit val rioLocalSpan: Local[H, Span[Task]] = Local[H, Env].focus(Env.span)
+  type H[x] = RIO[Clock with Blocking with Has[Env], x]
+  implicit val rioLocalSpan: Local[H, Span[RIO[Clock with Blocking, *]]] =
+    Local[H, Env].focus(Env.span)
   implicitly[Trace[H]]
+
+  type I[x] = RIO[Clock with Blocking with Console with Random with Has[Span[Effect]], x]
+  implicitly[Trace[I]].span("Hello")(RIO.succeed("World"))
 }
