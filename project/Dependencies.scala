f79cbdd433a721de70df2fbb12885b64562f41f1
Scala Steward <me@scala-steward.org>
2022-04-04 03:05:20 +0200

    Update cats-effect to 3.3.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
 
     val trace4cats = "0.13.1"
 
-    val catsEffect = "3.3.9"
+    val catsEffect = "3.3.10"
     val zioInteropCats = "3.2.9.1"
 
     val kindProjector = "0.13.2"
