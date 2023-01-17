5adf5c0e1d5d06d5217e91315af07b039b5f7030
Scala Steward <scala_steward@virtuslab.com>
2023-01-17 22:55:54 +0000

    Update cats-effect to 3.4.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
 
     val trace4cats = "0.14.0"
 
-    val catsEffect = "3.3.14"
+    val catsEffect = "3.4.5"
     val zioInteropCats = "3.2.9.1"
 
     val kindProjector = "0.13.2"
